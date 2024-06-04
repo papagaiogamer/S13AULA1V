1217c1d59c72e12351d6bfafa5986b73c10bbc18
papagaio <163757749+papagaiogamer@users.noreply.github.com>
2024-06-04 08:16:13 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+def greet(name):
+    return "sou produto de 6 milhões de anos de evolução?vamos lá cara.Eu rastejei de um pântano ontem\n-" + name 
+
+if __name__ == "__main__":
+    print(greet("DAVID"))
